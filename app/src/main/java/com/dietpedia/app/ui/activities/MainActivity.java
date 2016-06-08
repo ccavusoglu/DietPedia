bc99da396ae06733b8132aedc6ee14e1344aaf4e
Ç. Çavuşoğlu <cavusoglucagatay@gmail.com>
2016-06-08 19:19:32 +0300

    initial

---
+++ app/src/main/java/com/dietpedia/app/ui/activities/MainActivity.java
@@ -0,0 +1,37 @@
+package com.dietpedia.app.ui.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import com.dietpedia.app.R;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/com/dietpedia/app/ui/views/MvpView.java
@@ -0,0 +1,6 @@
+package com.dietpedia.app.ui.views;
+
+/**
+ * Created by ccavusoglu on 08.06.2016.
+ */
+public interface MvpView {}
