6b360c7dd04753c9d62c932c0ef770865ada9e1d
Ç. Çavuşoğlu <cavusoglucagatay@gmail.com>
2016-06-16 18:29:31 +0300

    some fonts

--- app/src/main/java/com/dietpedia/app/ui/activities/MainActivity.java
+++ app/src/main/java/com/dietpedia/app/ui/activities/MainActivity.java
@@ -27,11 +27,6 @@ public class MainActivity extends Activity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
         return super.onOptionsItemSelected(item);
     }
 }
